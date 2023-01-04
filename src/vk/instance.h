a168170b10cdffa35215a085157390b251977c80
hw762 <carldong762@gmail.com>
2023-01-04 15:06:02 -0500

    Initial commit

--- src/vk/instance.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <vulkan/vulkan.h>
-
-VkInstance newVkInstance(const char** extensions, uint32_t n_extensions);
-VkDebugUtilsMessengerEXT newVkDebugUtilsMessengerEXT(VkInstance instance);
-
