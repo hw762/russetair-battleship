e86006df441e6b4647e0e676db45c962f88a018d
hw762 <carldong762@gmail.com>
2023-01-12 15:53:17 -0500

    Display a window

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,22 @@ import gfx.Window
 import gfx.vk.Device
 import gfx.vk.Instance
 import gfx.vk.PhysicalDevice
+import org.lwjgl.glfw.GLFW
 
 fun main() {
     val window = Window("Russetair Battleship")
     val instance = Instance(true)
     val physicalDevice = PhysicalDevice.createPhysicalDevice(instance)
     val device = Device(instance, physicalDevice)
+
+    while (!window.shouldClose()) {
+        window.pollEvents()
+        if (window.isKeyPressed(GLFW.GLFW_KEY_ESCAPE)) {
+            GLFW.glfwSetWindowShouldClose(window.windowHandle, true)
+        }
+    }
+    device.cleanup()
+    physicalDevice.cleanup()
+    instance.cleanup()
+    window.cleanup()
 }
